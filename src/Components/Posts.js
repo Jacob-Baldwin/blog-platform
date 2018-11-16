08ec70299c75ccce2686574d024f8a763c7e7e8f
Jacob Baldwin <jacob.ryan.baldwin@gmail.com>
2018-11-16 09:49:28 -0700

    post sorting

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -10,6 +10,8 @@ class Posts extends React.Component {
     this.state = {
       posts: {}
     };
+
+    this.sortPosts = this.sortPosts.bind(this);
   }
 
   componentDidMount() {
@@ -33,19 +35,35 @@ class Posts extends React.Component {
       });
   }
 
+  sortPosts() {
+    let post_array = [];
+    Object.keys(this.state.posts).map((key, index) => {
+      post_array.push(this.state.posts[key])
+    })
+
+    post_array.sort(function(a,b){
+      return new Date(b.dateposted) - new Date(a.dateposted);
+    });
+
+    return post_array;
+  }
+
   render() {
     var render_post = (p) => {
+
+      let date = new Date(p.datemodified)
+
       return(
         <div className="Post">
           <h1>{p.title}</h1>
           <ReactMarkdown source={p.body}/>
-          <p className="Footnote">Last Modified: {Date(p.datemodified)}</p>
+          <p className="Footnote">Last Modified: {date.toDateString()}</p>
         </div>
       );
     }
 
-    let posts = Object.keys(this.state.posts).map((key, index) => {
-      return (render_post(this.state.posts[key]))
+    let posts = this.sortPosts().map((p) => {
+      return (render_post(p))
     });
 
     return (
